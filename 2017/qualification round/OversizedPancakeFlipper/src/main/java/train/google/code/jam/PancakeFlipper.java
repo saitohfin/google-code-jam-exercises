3ea442a0c7b667e4cc7c303d9b19ec0e358e6ad6
Javi G <javidesoft@gmail.com>
2018-01-15 01:15:58 +0100

    Added calculate_amount_steps_to_flip_a_happy_side_is_zero test

---
+++ 2017/qualification round/OversizedPancakeFlipper/src/main/java/train/google/code/jam/PancakeFlipper.java
@@ -0,0 +1,11 @@
+package train.google.code.jam;
+
+public class PancakeFlipper {
+    public PancakeFlipper(int i) {
+
+    }
+
+    public Integer flip_to_perfect_happy(String currentPancake) {
+        return null;
+    }
+}
