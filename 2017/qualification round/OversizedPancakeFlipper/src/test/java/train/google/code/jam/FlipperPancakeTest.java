50fa0055107643bea80ffcc84bac133dd0b1c4ad
alfredo.gil@elision <alfredo.gil@elision.eu>
2018-01-17 23:23:13 +0100

    Previous test was incomplete, I think we should start for the creation of the HouseofPancakes

--- 2017/qualification round/OversizedPancakeFlipper/src/test/java/train/google/code/jam/FlipperPancakeTest.java
+++ 2017/qualification round/OversizedPancakeFlipper/src/test/java/train/google/code/jam/FlipperPancakeTest.java
@@ -3,17 +3,17 @@ package train.google.code.jam;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
-
 class FlipperPancakeTest {
+
+    private static final int ONE_PANCAKE_SIZE =1;
 
     @Before
     public void setUp(){
 
     }
 
-    @Test
-    public void calculate_amount_steps_to_flip_a_happy_side_is_zero(){
+    /*@Test
+    public void calculate_amount_steps_to_flip_a_happy_side_is_zero() {
         //Arrange
         final Integer expected = 0;
         PancakeFlipper flipper = new PancakeFlipper(1);
@@ -22,5 +22,16 @@ class FlipperPancakeTest {
         Integer amount_steps = flipper.flip_to_perfect_happy(currentPancake);
         //Assert
         assertEquals("The amount of steps is incorrect", expected, amount_steps);
+    }*/
+
+    @Test(expected = PancakeHouseCreationException.class)//Expected
+    public void creatingAHouseOFPancakes_WithAPancakeFlipperSmallerThanTwo_ShouldThrownExceptions()
+    {
+        //Given
+        PancakeHouse houseOfPancakes;
+        PancakeFlipper pancakeFlipper = new PancakeFlipper(ONE_PANCAKE_SIZE);
+
+        //When
+        houseOfPancakes=new PancakeHouse(pancakeFlipper);
     }
 }
