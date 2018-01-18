543f5fda357eee94455e25d8a16f60e176d89dba
Javi G <javidesoft@gmail.com>
2018-01-18 02:05:07 +0100

    Refactor the flipper validation

--- 2017/qualification round/OversizedPancakeFlipper/src/main/java/train/google/code/jam/PancakeHouse.java
+++ 2017/qualification round/OversizedPancakeFlipper/src/main/java/train/google/code/jam/PancakeHouse.java
@@ -4,10 +4,15 @@ import train.google.code.jam.exceptions.PancakeHouseCreationException;
 
 public class PancakeHouse {
 
+    private static final Integer MINIM_FLIPPER_SIZE = 2;
     private final PancakeFlipper pancakeFlipper;
 
     public PancakeHouse(PancakeFlipper pancakeFlipper) throws PancakeHouseCreationException {
-        if(pancakeFlipper.size() < 2) throw new PancakeHouseCreationException();
+        validate_components(pancakeFlipper);
         this.pancakeFlipper = pancakeFlipper;
+    }
+
+    private void validate_components(PancakeFlipper pancakeFlipper) throws PancakeHouseCreationException {
+        if(pancakeFlipper.size() < MINIM_FLIPPER_SIZE) throw new PancakeHouseCreationException();
     }
 }
