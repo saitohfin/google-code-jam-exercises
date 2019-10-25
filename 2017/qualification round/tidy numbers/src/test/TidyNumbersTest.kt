aec890fe37d61273a4982c697f70182457aff80b
Javier Garcia Muñoz <javidesoft@gmail.com>
2019-10-25 02:19:22 +0200

    Basic kotlin structure

---
+++ 2017/qualification round/tidy numbers/src/test/TidyNumbersTest.kt
@@ -0,0 +1,13 @@
+package train.google.code.jam
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class TidyNumbersTest (){
+
+    @Test
+    fun f(){
+        assertEquals(true, true)
+    }
+
+}
