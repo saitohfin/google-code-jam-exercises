7ef7dae1901cd98895e82e888a6f4faa2675fc38
Javier Garcia Muñoz <javidesoft@gmail.com>
2019-10-25 16:26:23 +0200

    Work with Long type

--- 2017/qualification round/tidy numbers/src/test/TidyNumbersTest.kt
+++ 2017/qualification round/tidy numbers/src/test/TidyNumbersTest.kt
@@ -12,39 +12,46 @@ class TidyNumbersTest() {
 
     @Test
     fun numbers_lowers_than_ten_are_tidy() {
-        var numbers = listOf(0, 1, 2, 3, 4, 5, 6, 7, 8, 9)
         for (number in 0 until 10) {
-            val tidyNumber = TidyNumber(number)
-            assertEquals(number, tidyNumber.number())
+            val tidyNumber = TidyNumber(number.toLong())
+            assertEquals(number.toLong(), tidyNumber.number())
         }
     }
 
     @Test
     fun number_with_digits_are_higher_than_before_is_tidy() {
-        var number = 19
-        val tidyNumber = TidyNumber(number)
-        assertEquals(number, tidyNumber.number())
+        val number = 19
+        val tidyNumber = TidyNumber(number.toLong())
+        assertEquals(number.toLong(), tidyNumber.number())
     }
 
     @Test
     fun number_with_digits_are_equals_is_tidy() {
-        var number = 55
-        val tidyNumber = TidyNumber(number)
-        assertEquals(number, tidyNumber.number())
+        val number = 55
+        val tidyNumber = TidyNumber(number.toLong())
+        assertEquals(number.toLong(), tidyNumber.number())
     }
 
     @Test
     fun number_10_first_digit_is_higher_than_next_return_a_lower_tidynumber_9() {
-        var expected = 9
-        var number = 10
-        val tidyNumber = TidyNumber(number)
-        assertEquals(expected, tidyNumber.number())
+        val expected = 9
+        val number = 10
+        val tidyNumber = TidyNumber(number.toLong())
+        assertEquals(expected.toLong(), tidyNumber.number())
     }
 
     @Test
     fun number_132_digitS_not_shorted_return_a_lower_tidynumber_129() {
-        var expected = 129
-        var number = 132
+        val expected = 129
+        val number = 132
+        val tidyNumber = TidyNumber(number.toLong())
+        assertEquals(expected.toLong(), tidyNumber.number())
+    }
+
+    @Test
+    fun number_111111111111111110_digitS_not_shorted_return_a_lower_tidynumber_99999999999999999() {
+        val expected = 99999999999999999
+        val number = 111111111111111110
         val tidyNumber = TidyNumber(number)
         assertEquals(expected, tidyNumber.number())
     }
